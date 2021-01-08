92835bc040044a51e96c0e3470d68c79d27d294b
Tejas Doshi <tejasmdoshi@outlook.com>
2021-01-08 23:23:42 +0530

    new springboot web project

---
+++ src/main/java/com/sapient/WeatherAssessment/WeatherAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.WeatherAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/WeatherAssessment/WeatherAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.WeatherAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
